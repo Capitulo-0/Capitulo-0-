bf92ce81ad65b77dc8019ed67ab6d49e0a345bb6
zzzzouv <lalitocortes4488@gmail.com>
2026-09-01 17:39:11 -0400

    chore: crear estructura inicial de frontend y backend

---
+++ backend/src/test/java/com/capitulo0/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capitulo0.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
